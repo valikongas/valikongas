79557550fe867897965ad7daa3fad0cc3ae115d7
valikongas <gedas@vertintojas.lt>
2024-10-24 22:01:14 +0300

    11 pamoka

---
+++ 11 pamoka/11 pamoka/Program.cs
@@ -0,0 +1,72 @@
+﻿
+////Nr. 1.
+//int a = 10;
+//int b = 5;
+//int c = 12;
+
+//int max = a;
+//if (b > max)
+//    max = b;
+//if (c > max)
+//    max = c;
+
+//Console.WriteLine("The maximum value is: " + max);
+
+////Eil.Nr. 3
+//int counter = 0;
+//while(counter<=10)
+//{
+//    Console.WriteLine("Count: " + counter);
+//    counter++;
+//}
+
+////Nr. 5.
+//string name1 = "John";
+//string name2 = "john";
+//if (name1.Equals(name2))
+//{
+//    Console.WriteLine("Name are the same.");
+//}
+//else
+//{
+//    Console.WriteLine("Names are different.");
+//}
+
+
+using System.Diagnostics;
+using System.Text;
+
+StringBuilder sb = new StringBuilder("Pradinis tekstas aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+    // Pridėti tekstą
+    sb.Append(", pridėtas tekstas");
+
+    Console.WriteLine(sb.ToString());
+
+    // Įterpti tekstą
+    sb.Insert(8, " naujas");
+
+    Console.WriteLine(sb.ToString());
+
+    // Pašalinti dalį teksto
+    sb.Remove(0, 7);
+
+    Console.WriteLine(sb.ToString());
+
+    // Pakeisti dalį teksto
+    sb.Replace('a','x',20,10);
+
+
+    Console.WriteLine(sb.ToString());
+
+
+Stopwatch sw = new Stopwatch();
+sw.Start();
+
+sw.Stop();
+TimeSpan laikas = sw.Elapsed;
+Console.WriteLine(sw.Elapsed);
+Console.WriteLine(laikas);
+
+TimeSpan timeSpan = new TimeSpan();
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string str = "gedas dangus alytus kaunass";
+
+            // StringBuilder sb = new StringBuilder(str);
+
+            // for (int i=0; i<(sb.Length-1); i++)
+            // for (int j=i+1; j<sb.Length; j++)
+            // {
+            //     if (sb[i] == sb[j])
+            //     {
+            //             sb = sb.Remove(j, 1);
+            //             j -= 1;
+            //     }
+
+            // }
+            //Console.WriteLine(sb.ToString());
+
+            ////Nr. 1
+            // int[] masyvas = new int[10];
+
+            // for (int i = 0; i < masyvas.Length; i++)
+            // {
+            //     masyvas[i] = i+5;
+            // }
+            // int sum = 0;
+            // int max = 0;
+            // for (int i = 0; i < masyvas.Length; i++)
+            // {
+            //     masyvas[i] = masyvas[i] * masyvas[i];
+            //     Console.WriteLine(masyvas[i]);
+            //     sum += masyvas[i];
+            //     if (masyvas[i] > max)
+            //         max = masyvas[i];
+            // }
+            // Console.WriteLine($"Suma = {sum}");
+            // Console.WriteLine($"Max = {max}");
+
+            // int[] invertMasyvas = new int[10];
+            // for (int i = 0; i < invertMasyvas.Length; i++)
+            // {
+            //     invertMasyvas[i] = masyvas[9 - i];
+            //     Console.WriteLine(invertMasyvas[i]);
+            // }
+
+
+            ////Nr.2
+            //string text = "labas";
+            //char[] chars = text.ToCharArray();
+            //for (int i = 0; i < chars.Length; i++)
+            //{
+            //    Console.Write("'" + chars[i] + "', ");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine(chars[0]);
+            //Console.WriteLine(chars[chars.Length-1]);
+
+
+            //Nr.3.1-2
+            int[] masyvas = new int[50];
+            Random random = new Random();   
+            for (int i = 0; i < masyvas.Length; i++)
+            {  masyvas[i] = random.Next(1,100);
+                Console.Write(masyvas[i]+" ");
+            }
+            for (int j = 0; j < masyvas.Length; j++)
+            {
+                for (int i = 0; i < masyvas.Length - 1; i++)
+                {
+                    if (masyvas[i] > masyvas[i + 1])
+                    {
+                        int laikinas = masyvas[i];
+                        masyvas[i] = masyvas[i + 1];
+                        masyvas[i + 1] = laikinas;
+                    }
+                }
+            }
+                Console.WriteLine();
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+              
+                Console.Write(masyvas[i]+" ");
+            }
+
+
+            
+
+
+
+
+
+
+        }
+    }
+}
