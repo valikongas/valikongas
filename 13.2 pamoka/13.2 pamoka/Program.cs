f6ce6d9a9d1f91d965a831d0ffe659bee62b7e69
valikongas <gedas@vertintojas.lt>
2024-10-29 22:10:56 +0200

    14 pamoka

--- 13.2 pamoka/13.2 pamoka/Program.cs
+++ 13.2 pamoka/13.2 pamoka/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Channels;
+﻿using System.Runtime.Serialization;
+using System.Threading.Channels;
 
 namespace _13._2_pamoka
 {
@@ -12,10 +13,42 @@ namespace _13._2_pamoka
 
     public class Zaidimas
     {
+       
+        
+        
+
+
         public static void ZaidimoPradzia()
         {
-            NupieskLauka();
+          int[,] laukas = new int[3, 3];
+          
+            Console.Clear();
+            Console.WriteLine("Kiek zaideju zais 1 ar 2: ");
+            bool isIntenger1 = false;
+            int x1 = 0;
+            do
+            {
+                isIntenger1 = int.TryParse(Console.ReadLine(), out x1);
+                if (isIntenger1 && (x1 <= 0 || x >= 3))
+                    isIntenger1 = false;
+            }
+            while (!isIntenger1);
+
+            switch(x1)
+            {
+                case (1):
+
+                    break;
+                    case (2):
+                    break;
+            }
+
+
+
+
+            NupieskLauka(laukas);
             IveskKoordinates(out int x, out int y);
+            
 
        
         }
@@ -60,6 +93,107 @@ namespace _13._2_pamoka
 
         
         }
+
+        public static void NupieskLauka(int[,] laukas)
+        {
+            Console.Clear();
+
+            Console.WriteLine("     Virsuje - X, sone - Y");
+            Console.WriteLine("      1         2         3");
+
+            for (int i = 0; i < 3; i++)
+            {
+
+
+                if (laukas[0, i] == 0 && laukas[1, i] == 0 && laukas[2, i] == 0)
+
+                {
+                    for (int j = 0; j < 8; j++)
+                        if (j == 4)
+                        {
+                            Console.WriteLine($"{i+1}         ||        ||        ");
+                        }
+                    else 
+                    { Console.WriteLine("          ||        ||        "); }
+                }
+                else if (laukas[0, i] == 1 && laukas[1, i] == 0 && laukas[2, i] == 0)
+                {
+                    Console.WriteLine("          ||        ||        ");
+                    Console.WriteLine("   X    X ||        ||        ");
+                    Console.WriteLine("    X  X  ||        ||        ");
+                    Console.WriteLine($"{i+1}  XX   ||        ||        ");
+                    Console.WriteLine("     XX   ||        ||        ");
+                    Console.WriteLine("    X  X  ||        ||        ");
+                    Console.WriteLine("   X    X ||        ||        ");
+                    Console.WriteLine("          ||        ||        ");
+                }
+                else if (laukas[0, i] == 2 && laukas[1, i] == 0 && laukas[2, i] == 0)
+                {
+                    Console.WriteLine("          ||        ||        ");
+                    Console.WriteLine("     XX   ||        ||        ");
+                    Console.WriteLine("    X  X  ||        ||        ");
+                    Console.WriteLine($"{i+1}X    X ||        ||        ");
+                    Console.WriteLine("   X    X ||        ||        ");
+                    Console.WriteLine("    X  X  ||        ||        ");
+                    Console.WriteLine("     XX   ||        ||        ");
+                    Console.WriteLine("          ||        ||        ");
+                }
+                else if (laukas[0, i] == 0 && laukas[1, i] == 1 && laukas[2, i] == 0)
+                {
+                    Console.WriteLine("          ||        ||        ");
+                    Console.WriteLine("          || X    X ||        ");
+                    Console.WriteLine("          ||  X  X  ||        ");
+                    Console.WriteLine($"{i+1}       ||   XX   ||        ");
+                    Console.WriteLine("          ||   XX   ||        ");
+                    Console.WriteLine("          ||  X  X  ||        ");
+                    Console.WriteLine("          || X    X ||        ");
+                    Console.WriteLine("          ||        ||        ");
+                }
+                else if (laukas[0, i] == 2 && laukas[1, i] == 2 && laukas[2, i] == 0)
+                {
+                    Console.WriteLine("        ||        ||        ");
+                    Console.WriteLine("        ||   XX   ||        ");
+                    Console.WriteLine("        ||  X  X  ||        ");
+                    Console.WriteLine($"{i+1}       || X    X ||        ");
+                    Console.WriteLine("        || X    X ||        ");
+                    Console.WriteLine("        ||  X  X  ||        ");
+                    Console.WriteLine("        ||   XX   ||        ");
+                    Console.WriteLine("        ||        ||        ");
+                }
+                else if (laukas[0, i] == 0 && laukas[1, i] == 0 && laukas[2, i] == 1)
+                {
+                    Console.WriteLine("        ||        ||        ");
+                    Console.WriteLine("        ||        || X    X ");
+                    Console.WriteLine("        ||        ||  X  X  ");
+                    Console.WriteLine($"{i+1}       ||        ||   XX   ");
+                    Console.WriteLine("        ||        ||   XX   ");
+                    Console.WriteLine("        ||        ||  X  X  ");
+                    Console.WriteLine("        ||        || X    X ");
+                    Console.WriteLine("        ||        ||        ");
+                }
+                else if (laukas[0, i] == 2 && laukas[1, i] == 0 && laukas[2, i] == 2)
+                {
+                    Console.WriteLine("        ||        ||        ");
+                    Console.WriteLine("        ||        ||   XX   ");
+                    Console.WriteLine("        ||        ||  X  X  ");
+                    Console.WriteLine($"{i+1}       ||        || X    X ");
+                    Console.WriteLine("        ||        || X    X ");
+                    Console.WriteLine("        ||        ||  X  X  ");
+                    Console.WriteLine("        ||        ||   XX   ");
+                    Console.WriteLine("        ||        ||        ");
+                }
+                if (i != 2)
+                {
+                    Console.WriteLine("  ____________________________");
+                    Console.WriteLine("  ____________________________");
+                }
+            }
+
+        }
+
+
+
+
 
     }
 
---
+++ 14  pamoka/14  pamoka/Program.cs
@@ -0,0 +1,121 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace _14__pamoka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> sarasas = new List<string> { "4", "5", "pirmas", "tomas", "antanas", "kaunas", "vilnius"};
+            
+            PrintList(sarasas);
+
+            List<int> numbers = new List<int>();
+            UzpildymasSkaiciais(numbers);
+            SkaiciuVidurkis (numbers);
+            Skaiciaididesninei10(numbers);
+            Raidziusuma(sarasas);
+            Pirminiai(sarasas);
+
+        }
+
+
+
+
+
+        static void PrintList(List<string> sarasas)
+        {
+            foreach (string s in sarasas)
+            {
+                Console.WriteLine(s + "- ilgis: " + s.Length);
+            }
+
+        }
+
+        static void UzpildymasSkaiciais(List<int> numbers)
+        { 
+        for(int i=0;i<=50;i++)
+            {
+                numbers.Add(i);
+            }
+        
+        }
+
+        static void SkaiciuVidurkis(List<int> numbers)
+        {
+            Console.WriteLine("Skaiciu vidurkis yra: "+numbers.Average());
+        }
+
+        static void Skaiciaididesninei10(List<int> numbers)
+        {
+            numbers.Add (0);
+            for (int i=numbers.Count-1;i>=0;i--)
+            {
+                
+                if (numbers[i] <= 10)
+                { numbers.Remove(numbers[i]);
+                                        
+                }
+            }
+            foreach (int i in numbers)
+                Console.WriteLine(i);
+        
+        }
+
+
+        static void Raidziusuma(List<string> sarasas )
+        {
+             List<string> naujassarasas= new List<string>();
+            foreach (string i in sarasas)
+            {
+                int suma = 0;
+                foreach (char s in i)
+                {
+                    suma=suma+(int)s;
+                }
+               
+
+                if (suma%2==0)
+                        naujassarasas.Add(i);
+            }
+
+            foreach(string i in naujassarasas)
+            { Console.WriteLine(i); }
+
+        }
+
+        static void Pirminiai(List<string> sarasas)
+        {
+            List<string> naujassarasas1 = new List<string>();
+            foreach (string i in sarasas)
+            {
+                int suma = 0;
+                foreach (char s in i)
+                {
+                    suma = suma + (int)s;
+                }
+                Console.WriteLine(suma);
+                bool isGeras = true;
+                for (int j = 2; j < suma - 1; j++)
+                {
+                    if (suma % j == 0)
+                    {
+                        isGeras = false;
+                        break;
+                    }
+
+                }
+                if (isGeras)
+                    naujassarasas1.Add(i);
+            }
+                foreach (string k in naujassarasas1)
+                {
+                    Console.WriteLine(k);
+                }
+            
+        }
+
+
+    }
+}
